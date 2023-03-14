b3463b2aec2ffe712d77f3fd9543df9d236b809b
mmunkyeong <jmk4379@naver.com>
2023-03-14 16:04:39 +0900

    count 증가/ 세션 추가

--- src/main/java/com/ll/basic1/HomeController.java
+++ src/main/java/com/ll/basic1/HomeController.java
@@ -1,5 +1,8 @@
 package com.ll.basic1;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,7 +12,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Controller
@@ -45,13 +50,13 @@ public class HomeController {
     public String showMain3(){
         return "즐거웠습니다.";
     }
+
     @GetMapping("/home/increase")
     @ResponseBody
     public int showIncrease(){ // 리턴되는 int 값은 String화 되어서 출력
         count++;
         return count;
     }
-
     @GetMapping("/home/plus")
     @ResponseBody
     //int a는 쿼리스트링에서 a파라미터의 값을 정수화한 값이어야 한다.
@@ -100,7 +105,39 @@ public class HomeController {
     public String showPerson(){
         return list.toString();
     }
+
+    @GetMapping("/home/reqAndResp")
+    @ResponseBody
+    public void showReqAndResp(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        int age = Integer.parseInt(req.getParameter("age"));
+        resp.getWriter().append("Hello, you are %d years old.".formatted(age));
+    }
+
+    @GetMapping("/home/cookie/increase")
+    @ResponseBody
+    public int showCookieIncrease(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        int countInCookie = 0;
+
+        if (req.getCookies() != null) {
+            countInCookie = Arrays.stream(req.getCookies())
+                    .filter(cookie -> cookie.getName().equals("count"))
+                    .map(cookie -> cookie.getValue())
+                    .mapToInt(Integer::parseInt)
+                    .findFirst()
+                    .orElse(0);
+        }
+
+        int newCountInCookie = countInCookie + 1;
+
+        resp.addCookie(new Cookie("count", newCountInCookie + ""));
+
+        return newCountInCookie;
+    }
 }
+
+
+
+
 @AllArgsConstructor //모든 생성자
 @Getter // 따로 선언하지 않아도 Get 생성
 @Setter //set도 마찬가지
@@ -116,6 +153,7 @@ class Person{
 Person(String name,int age){
     this(++lastid, name,age);
 }
+
     Person(int id){
         this.id=id;
     }
