3bd963778d00a45cf12f8f21fde765ea13f28fd8
mmunkyeong <jmk4379@naver.com>
2023-03-17 22:56:18 +0900

    member를 엔티티로 변경

--- src/main/java/com/ll/basic1/boundedContext/member/entity/Member.java
+++ src/main/java/com/ll/basic1/boundedContext/member/entity/Member.java
@@ -1,26 +1,34 @@
 package com.ll.basic1.boundedContext.member.entity;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@NoArgsConstructor
 @AllArgsConstructor
 //@Data //getter, setter, tostring 등등 다 들어간 것
 @Getter
 @ToString
+@Entity
+@EntityListeners(AuditingEntityListener.class)
 public class Member {
-    private static long lastId;
-    private final long id;
-    private final String username;
-    private final String password;
-
-
-    static {
-        lastId = 0;
-    }
-
-    public Member(String username, String password) {
-        this(++lastId, username, password);
-    }
+    @Id // PRIMARY KEY
+    @GeneratedValue(strategy = IDENTITY) // AUTO_INCREMENT
+    private long id;
+    @CreatedDate
+    private LocalDateTime createDate; // 데이터 생성 날짜
+    @LastModifiedDate
+    private LocalDateTime modifyDate; // 데이터 수정 날짜
+    @Column(unique = true)
+    private String username;
+    private String password;
 }
--- src/main/java/com/ll/basic1/boundedContext/member/service/MemberService.java
+++ src/main/java/com/ll/basic1/boundedContext/member/service/MemberService.java
@@ -16,7 +16,7 @@ public class MemberService {
     private final MemberRepository memberRepository;
 
     public RsData tryLogin(String username, String password) {
-        Member member = memberRepository.findByUsername(username);
+        Member member = memberRepository.findByUsername(username).orElse(null);
 
         if (member == null) {
             return RsData.of("F-2", "%s(은)는 존재하지 않는 회원입니다.".formatted(username));
@@ -30,9 +30,9 @@ public class MemberService {
     }
 
     public Member findByUsername(String username) {
-        return memberRepository.findByUsername(username);
+        return memberRepository.findByUsername(username).orElse(null);
     }
     public Member findById(long id) {
-        return memberRepository.findById(id);
+        return memberRepository.findById(id).orElse(null);
     }
 }
