dc194a734e63af3d55172c3a5cef62d5628d9297
mmunkyeong <jmk4379@naver.com>
2023-03-17 11:17:21 +0900

    article 클래스 수정

--- src/main/java/com/ll/basic1/boundedContext/article/entity/Article.java
+++ src/main/java/com/ll/basic1/boundedContext/article/entity/Article.java
@@ -1,12 +1,20 @@
 package com.ll.basic1.boundedContext.article.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 
-@Entity
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity // Entity라는 것을 알려줌
 public class Article {
     @Id //primary key 설정
+    @GeneratedValue(strategy = IDENTITY) //auto_increment
     private long id;
+    private LocalDateTime createDate; // 데이터 생성 날짜
+    private LocalDateTime  modifyDate; // 데이터 생성 날짜
     private String title;
     private String body;
 }
